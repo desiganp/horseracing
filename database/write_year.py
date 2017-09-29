739c65df2e4428966ee8f3fe6a78aeb233700350
desiganp <desigan.padayachee@gmail.com>
2017-09-29 20:22:14 +0200

    Create write_year.py

---
+++ database/write_year.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 17:52:16 2017
+
+@author: Desigan
+"""
+def write_year(year):
+    
+    url_list = []
+
+    if year == 2009:
+#        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=01/2009')
+#        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=02/2009')
+#        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=03/2009')
+#        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=04/2009')
+#        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=05/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=06/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=07/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=08/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=09/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=10/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=11/2009')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2009-Full-Results?racesformonth=12/2009')
+
+    elif year == 2010:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=01/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=02/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=03/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=04/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=05/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=06/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=07/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=08/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=09/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=10/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=11/2010')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2010-Full-Results?racesformonth=12/2010')
+
+    elif year == 2011:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=01/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=02/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=03/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=04/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=05/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=06/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=07/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=08/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=09/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=10/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=11/2011')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2011-Full-Results?racesformonth=12/2011')
+
+    elif year == 2012:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=01/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=02/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=03/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=04/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=05/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=06/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=07/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=08/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=09/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=10/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=11/2012')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2012-Full-Results?racesformonth=12/2012')
+
+    elif year == 2013:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=01/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=02/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=03/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=04/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=05/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=06/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=07/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=08/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=09/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=10/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=11/2013')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2013-Full-Results?racesformonth=12/2013')
+    
+    elif year == 2014:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=01/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=02/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=03/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=04/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=05/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=06/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=07/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=08/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=09/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=10/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=11/2014')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2014-Full-Results?racesformonth=12/2014')
+    
+    elif year == 2015:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=01/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=02/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=03/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=04/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=05/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=06/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=07/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=08/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=09/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=10/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=11/2015')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2015-Full-Results?racesformonth=12/2015')
+
+    elif year == 2016:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=01/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=02/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=03/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=04/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=05/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=06/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=07/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=08/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=09/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=10/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=11/2016')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2016-Full-Results?racesformonth=12/2016')
+
+    elif year == 2017:
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=01/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=02/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=03/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=04/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=05/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=06/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=07/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=08/2017')
+        url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=09/2017')
+        #url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=10/2017')
+        #url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=11/2017')
+        #url_list.append('http://news.tabonline.co.za/Full-Results/2017-Full-Results?racesformonth=12/2017')
+        
+    else:
+        print('No data for specified year')
+        return
+
+
+#%%
+    import sqlite3 as sql
+    import pandas as pd
+    #from parse_xml import parse_xml
+    from write_month import write_month_data
+    
+    for url in url_list:
+        df = write_month_data(url,year)
+        con = sql.connect("racedata.db")
+        for d in df:
+            pd.io.sql.to_sql(d, ('results_v1_' + str(year)), con, if_exists='append',)
